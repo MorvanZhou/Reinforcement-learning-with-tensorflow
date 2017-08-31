9e39a67c2cd8a417dfb70a6f46b340f8856299f5
morvan Zhou <morvanzhou@gmail.com>
2017-08-31 14:26:32 +1000

    fix mistake

--- contents/5_Deep_Q_Network/DQN_modified.py
+++ contents/5_Deep_Q_Network/DQN_modified.py
@@ -83,9 +83,9 @@ class DeepQNetwork:
 
         # ------------------ build target_net ------------------
         with tf.variable_scope('target_net'):
-            e1 = tf.layers.dense(self.s, 20, tf.nn.relu, kernel_initializer=w_initializer,
+            t1 = tf.layers.dense(self.s_, 20, tf.nn.relu, kernel_initializer=w_initializer,
                                  bias_initializer=b_initializer)
-            self.q_next = tf.layers.dense(e1, self.n_actions, kernel_initializer=w_initializer,
+            self.q_next = tf.layers.dense(t1, self.n_actions, kernel_initializer=w_initializer,
                                           bias_initializer=b_initializer)
 
         with tf.variable_scope('q_target'):
