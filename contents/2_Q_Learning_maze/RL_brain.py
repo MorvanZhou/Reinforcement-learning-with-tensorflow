1faef74e7ddf364f30ed052c3a8b50ab919aa8a1
morvanzhou <morvanzhou@gmail.com>
2018-09-03 11:10:24 +0800

    fix selecting action

--- contents/2_Q_Learning_maze/RL_brain.py
+++ contents/2_Q_Learning_maze/RL_brain.py
@@ -23,8 +23,8 @@ class QLearningTable:
         if np.random.uniform() < self.epsilon:
             # choose best action
             state_action = self.q_table.loc[observation, :]
-            state_action = state_action.reindex(np.random.permutation(state_action.index))     # some actions have same value
-            action = state_action.idxmax()
+            # some actions may have the same value, randomly choose on in these actions
+            action = np.random.choice(state_action[state_action == np.max(state_action)].index)
         else:
             # choose random action
             action = np.random.choice(self.actions)
